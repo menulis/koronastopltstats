eeaccb9f91728b924daf5c33b7699e4ab059f38e
Džiugas Baltrūnas <dziugas.baltrunas@forkbeardtech.com>
2020-11-06 13:39:07 +0100

    Prievado nuskaitymas iš PORT environment

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use('/data', async function(req, res) {
 )
 
 const server = http.createServer(app)
-const port = 3000
+const port = process.env.PORT || 3000
 server.listen(port)
 console.debug('Server listening on port ' + port)
 
