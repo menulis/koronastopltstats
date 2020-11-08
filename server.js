4e185de8ce5459420db1b8a4fe2e5b0c53c60748
Džiugas Baltrūnas <dziugas.baltrunas@forkbeardtech.com>
2020-11-08 13:14:34 +0100

    Pakeistas URL
    Taip pat bukete ieškoma valandinių raktų.

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const http = require('http')
 const request = require('request')
 const fetch = require('node-fetch')
 const express = require('express')
-const path = require('path')
 const app = express()
 const unzipper = require('unzipper')
 const protobuf = require('protobufjs')
+var xpath = require('xpath')
+var dom = require('xmldom').DOMParser
+
+const url = 'https://download.koronastop.lt'
  
 app.use(express.json())
 app.use(express.static('express'))
@@ -13,21 +16,27 @@ app.use(express.static('.'))
 app.use('/data', async function(req, res) {
     protobuf.load("gaen.proto", async function (err, root) {
       var counters = {}
+      // res.send([{"date": "2020-10-10T12:00:00", "count": 1}, {"date": "2020-10-10T14:00:00", "count": 3}])
       try {
         var TemporaryExposureKeyExport = root.lookupType("TemporaryExposureKeyExport")
-        var dateResp = await fetch('https://download.cwa.kryptis.lt/version/v1/diagnosis-keys/country/LT/date')
-        var dates = await dateResp.json()
-
-        for (const date of dates) {
-          var keys = await getFile(`https://download.cwa.kryptis.lt/version/v1/diagnosis-keys/country/LT/date/${date}`, 'export.bin')
-          var keyExport = TemporaryExposureKeyExport.decode(keys)
-          counters[date] = keyExport["keys"].length
+        var select = xpath.useNamespaces({"s3": "http://s3.amazonaws.com/doc/2006-03-01/"});
+        var listBucketResult = await fetch(url)
+        if (!listBucketResult.ok) {
+          throw new Error(listBucketResult.statusText)
         }
+        var xml = await listBucketResult.text()
+        var doc = await new dom().parseFromString(xml)
+        var hourlyKeyFiles = await select("//s3:Key[contains(text(), '/hour/')]/text()", doc)
+          .map(node => node.nodeValue)
+          .sort(function(a, b) { return extractDate(a) > extractDate(b) ? 1 : -1 })
+          .map(hourlyKeyFile => extractKeyCount(url, hourlyKeyFile, 'export.bin', TemporaryExposureKeyExport))
+        var counters = await Promise.all(hourlyKeyFiles)
+        res.send(counters)
       } catch (e) {
         console.log(e)
+        res.statusMessage = e
+        res.status(500).end()
       }
-
-      res.send(counters)
 
     })
   }
@@ -38,10 +47,19 @@ const port = process.env.PORT || 3000
 server.listen(port)
 console.debug('Server listening on port ' + port)
 
-async function getFile(url, fileName) {
-  const directory = await unzipper.Open.url(request, url)
+async function extractKeyCount(url, path, fileName, TemporaryExposureKeyExport) {
+  const directory = await unzipper.Open.url(request, url + '/' + path)
   const file = directory.files.find(d => d.path === fileName)
   const content = await file.buffer()
-  return content.slice(16)
+  const keys = content.slice(16)
+  const hour = extractDate(path)
+  var keyExport = TemporaryExposureKeyExport.decode(keys)
+  return { "date": hour, "count": keyExport["keys"].length }
 }
 
+function extractDate(keyValue) {
+  var elems = keyValue.split("/")
+  var hour = `0${elems[elems.length - 1]}:00:00`.slice(-8)
+  var date = elems[elems.length - 3]
+  return `${date}T${hour}`
+}
