5f4d8f5a21fc9f6bd1d69b06e342925d6996a21a
Džiugas Baltrūnas <dziugas.baltrunas@forkbeardtech.com>
2020-11-13 12:36:17 +0100

    Naudoti undici HTTP klientą

--- server.js
+++ server.js
@@ -1,61 +1,52 @@
 const http = require('http')
-const request = require('request')
-const fetch = require('node-fetch')
 const express = require('express')
 const app = express()
 const unzipper = require('unzipper')
 const protobuf = require('protobufjs')
-const { response } = require('express')
+const { Client, Pool } = require('undici')
+const getStream = require('get-stream')
 
 const urls = {
   'LT': 'https://download.koronastop.lt',
   'DE': 'https://svc90.main.px.t-online.de'
 }
-
-const maxRequests = {
-  'LT': 10,
-  'DE': 100
-}
  
 app.use(express.json())
-app.use(express.static('express'))
 app.use(express.static('.'))
 app.get('/data/:countryCode/:type', async function(req, res) {
     protobuf.load("gaen.proto", async function (err, root) {
       const cc = req.params.countryCode
       const type = req.params.type
       const url = urls[cc]
+      const client = new Client(url, { pipelining: 10 } )
       try {
         const TemporaryExposureKeyExport = root.lookupType("TemporaryExposureKeyExport")
-        const datesResp = await fetch(`${url}/version/v1/diagnosis-keys/country/${cc}/date`)
-        if (!datesResp.ok) {
-          throw new Error(datesResp.statusText)
+        const { statusCode, headers, body } = await client.request({
+          path: `/version/v1/diagnosis-keys/country/${cc}/date`,
+          method: 'GET'
+        })
+
+        if (statusCode != 200) {
+          throw new Error(statusCode)
         }
-        const dates = await datesResp.json()
-        const dailyFiles = dates.map(date => `${url}/version/v1/diagnosis-keys/country/${cc}/date/${date}`)
+
+        const dates = JSON.parse(await getStream(body))
+        const dailyFiles = dates.map(date => `/version/v1/diagnosis-keys/country/${cc}/date/${date}`)
 
         const hourlyFiles = [].concat.apply([], await Promise.all(
           dailyFiles
             .map(dailyFile =>
-              fetch(`${dailyFile}/hour`)
-                .then(response => response.json())
-                .then(hours => hours.map(hour => `${dailyFile}/hour/${hour}`))
+              client.request({ path: `${dailyFile}/hour`, method: 'GET' })
+                .then(resp => getStream(resp.body))
+                .then(body => JSON.parse(body).map(hour => `${dailyFile}/hour/${hour}`))
             )
         ))
+
         const files = type == "hourly" ? hourlyFiles : dailyFiles
 
-        let requests = []
-        let counters = []
-        while (files.length) {
-          requests.push(files.shift())
-          if (requests.length == maxRequests[cc] || files.length == 0) {
-            let partialCounters = await Promise.all(
-              requests.map(file => extractKeyCount(file, 'export.bin', TemporaryExposureKeyExport))
-            )
-            counters.push(...partialCounters)
-            requests = []
-          }
-        }
+        let counters = await Promise.all(
+          files.map(path => extractKeyCount(client, path, 'export.bin', TemporaryExposureKeyExport))
+        )
 
         res.send(counters)
       } catch (e) {
@@ -63,7 +54,6 @@ app.get('/data/:countryCode/:type', async function(req, res) {
         res.statusMessage = e
         res.status(500).end()
       }
-
     })
   }
 )
@@ -73,13 +63,22 @@ const port = process.env.PORT || 3000
 server.listen(port)
 console.debug('Server listening on port ' + port)
 
-async function extractKeyCount(url, fileName, TemporaryExposureKeyExport) {
-  const directory = await unzipper.Open.url(request, url)
-  const file = directory.files.find(d => d.path === fileName)
-  const content = await file.buffer()
-  const keys = content.slice(16)
-  const temporaryExposureKeyExport = TemporaryExposureKeyExport.decode(keys)
-  return { "date": extractDate(url), "count": temporaryExposureKeyExport["keys"].length + temporaryExposureKeyExport["revisedKeys"].length }
+async function extractKeyCount(client, path, fileName, TemporaryExposureKeyExport) {
+  const temporaryExposureKeyExport = await client
+    .request({ path: path, method: 'GET', idempotent: true })
+    .then(resp => {
+      if (resp.statusCode != 200) {
+        throw new Error(`Klaida ${resp.statusCode}`)
+      }
+      return resp.body.pipe(unzipper.ParseOne(fileName))
+    })
+    .then(content => getStream.buffer(content))
+    .then(buffer => TemporaryExposureKeyExport.decode(buffer.slice(16)))
+
+    return {
+      "date": extractDate(path),
+      "count": temporaryExposureKeyExport["keys"].length + temporaryExposureKeyExport["revisedKeys"].length
+    }
 }
 
 function extractDate(keyValue) {
